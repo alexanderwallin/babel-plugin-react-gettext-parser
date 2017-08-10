aef63332b058eedc88b0f1ae4e7f6a3f90ef7e83
Alexander Wallin <office@alexanderwallin.com>
2017-08-10 20:43:13 +0100

    Removes console.log() thingies

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-
 var fs = require('fs');
 var path = require('path');
 var deepExtend = require('deep-extend');
 var reactGettextParser = require('react-gettext-parser');
 
 module.exports = function(babel) {
-  //console.log(babel);
   var allMessages = [];
 
   var traverser = reactGettextParser.getTraverser(function(messages, state) {
-    console.log('got messages', messages);
     allMessages = reactGettextParser.getUniqueBlocks(allMessages.concat(messages));
 
     reactGettextParser.outputPot(
